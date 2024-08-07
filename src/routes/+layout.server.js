4681397b1cba52c38214d766788c9e3f696c5483
intellltech <danieljames.maxdev@gmail.com>
2024-08-07 12:28:53 -0700

    Update +layout.server.js

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,6 @@
+import { MINIAPP_URL } from '$env/static/private'
 import { error } from '@sveltejs/kit'
+import { decrypt } from '$server/auth.js'
 
 /** @type {import('./$types').LayoutServerLoad} */
 export const load = async ({ cookies, url }) => {
@@ -6,4 +8,8 @@ export const load = async ({ cookies, url }) => {
 	const session = cookies.get('session')
 
 	if(!x || !session) error(500, '.')
+		return {
+			x: JSON.parse(await decrypt(x)),
+			MiniApp_URL: MINIAPP_URL
+		}
 }
